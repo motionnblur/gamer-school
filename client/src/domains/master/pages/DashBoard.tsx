3004c17645ff28266a3790c41bb5a1b696479e47
motionnblur <carbonfiberdev@gmail.com>
2025-10-27 01:57:12 +0300

    Improve and refactor

--- client/src/domains/master/pages/DashBoard.tsx
+++ client/src/domains/master/pages/DashBoard.tsx
@@ -5,11 +5,17 @@ import PersonIcon from "@mui/icons-material/Person";
 import TheatersIcon from "@mui/icons-material/Theaters";
 
 import React, { useState } from "react";
+import Videos from "./Videos";
+import Profile from "./Profile";
+import MainDashPage from "./MainDashPage";
 
 export default function DashBoard() {
-  const [disabledButton, setDisabledButton] = useState<String>("dashboard");
-  const handleClick = (buttonName: String) => {
-    setDisabledButton(buttonName);
+  const [disabledButton, setDisabledButton] = useState<Number>();
+  const [currentSelectedPageIndex, setCurrentSelectedPageIndex] =
+    useState<Number>(0);
+  const handleClick = (buttonId: Number) => {
+    setCurrentSelectedPageIndex(buttonId);
+    setDisabledButton(buttonId);
   };
 
   return (
@@ -26,40 +32,41 @@ export default function DashBoard() {
       >
         <Stack direction={"column"} width={"80%"} height={"86%"} spacing={2}>
           <Button
-            onClick={() => handleClick("dashboard")}
+            onClick={() => handleClick(0)}
             startIcon={<TvIcon />}
             sx={{
               color: "black",
               justifyContent: "flex-start",
               textAlign: "left",
-              pointerEvents: disabledButton === "dashboard" ? "none" : "auto",
-              boxShadow: disabledButton === "dashboard" ? 1 : 0,
+              pointerEvents: disabledButton === 0 ? "none" : "auto",
+              boxShadow: disabledButton === 0 ? 1 : 0,
             }}
           >
             Dashboard
           </Button>
           <Button
-            onClick={() => handleClick("profile")}
+            onClick={() => handleClick(1)}
             startIcon={<PersonIcon />}
             sx={{
               color: "black",
               justifyContent: "flex-start",
               textAlign: "left",
-              pointerEvents: disabledButton === "profile" ? "none" : "auto",
-              boxShadow: disabledButton === "profile" ? 1 : 0,
+              pointerEvents: disabledButton === 1 ? "none" : "auto",
+              boxShadow: disabledButton === 1 ? 1 : 0,
             }}
           >
             Profile
           </Button>
           <Button
-            onClick={() => handleClick("videos")}
+            id="2"
+            onClick={() => handleClick(2)}
             startIcon={<TheatersIcon />}
             sx={{
               color: "black",
               justifyContent: "flex-start",
               textAlign: "left",
-              pointerEvents: disabledButton === "videos" ? "none" : "auto",
-              boxShadow: disabledButton === "videos" ? 1 : 0,
+              pointerEvents: disabledButton === 2 ? "none" : "auto",
+              boxShadow: disabledButton === 2 ? 1 : 0,
             }}
           >
             Videos
@@ -83,7 +90,11 @@ export default function DashBoard() {
             backgroundColor: "white",
             borderRadius: "5px",
           }}
-        ></Stack>
+        >
+          {currentSelectedPageIndex === 0 && <MainDashPage />}
+          {currentSelectedPageIndex === 1 && <Profile />}
+          {currentSelectedPageIndex === 2 && <Videos />}
+        </Stack>
       </Box>
     </Stack>
   );
---
+++ client/src/domains/master/pages/MainDashPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MainDashPage() {
+  return <div>MainDashPage</div>;
+}
